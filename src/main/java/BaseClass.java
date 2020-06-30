d128b2a8bb346260089d92bed0fe90b9d1691d66
Yan <yan.bondarenko@gmail.com>
2020-06-30 23:58:03 +0300

    Add new capabilities

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -22,6 +22,8 @@ public class BaseClass {
 //        chromeOptions.addArguments("--disable-gpu");
 //        chromeOptions.addArguments("--disable-infobars");
         chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("window-size=1024,768\"");
 
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
