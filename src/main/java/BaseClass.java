5a49f1cd8e95f1074bf77f3ceb870317d92f407a
Yan <yan.bondarenko@gmail.com>
2020-06-30 23:50:18 +0300

    Add 'ChromeOptions' capabilities

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +14,16 @@ public class BaseClass {
 
     @Before
     public void start() {
-//        System.setProperty("webdriver.chrome.driver", "src/main/resources/mac/chromedriver");
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/linux/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/mac/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/linux/chromedriver");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--disable-infobars");
+        chromeOptions.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://google.com");
     }
